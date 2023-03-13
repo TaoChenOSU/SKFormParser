a518bc2fab66d5b759f2e29cc7d381980c9265de
Tao Chen <taochen@microsoft.com>
2023-03-13 16:34:52 -0700

    Remove hardcoded image path

--- Program.cs
+++ Program.cs
@@ -13,33 +13,37 @@ namespace SKFormParser
                 description: "The path to the settings file containing the endpoints and the keys to the services."
             );
 
+            var localImageOption = new Option<FileInfo?>(
+                name: "--local-image",
+                description: "The path to an image of a recept stored locally."
+            );
+
             // var setSettingsFileOption = new Option<FileInfo?>(
             //     name: "--set-settings",
             //     description: "Remember the path to the settings file containing the endpoints and the keys to the services."
             // );
 
-            var rootCommand = new RootCommand(
-                "Experimental app for form parsing using the Semantic Kernel."
-            );
-            rootCommand.AddOption(settingsFileOption);
-            // rootCommand.AddOption(setSettingsFileOption);
-
+            var receiptCommand = new Command("receipt", "Parse a receipt."){settingsFileOption, localImageOption};
             Settings? settings = null;
-            rootCommand.SetHandler(
-                (file) => {
-                    settings = Settings.FromJson(File.ReadAllText(file!.FullName)); 
+            FileInfo? imageLocal = null;
+            receiptCommand.SetHandler(
+                (settingsFile, localImageOption) => {
+                    settings = Settings.FromJson(File.ReadAllText(settingsFile!.FullName));
+                    imageLocal = localImageOption;
                 },
-                settingsFileOption
+                settingsFileOption, localImageOption
             );
 
-            // rootCommand.SetHandler(
-            //     (file) => {},
-            //     setSettingsFileOption
-            // );
+            var rootCommand = new RootCommand("Experimental app for form parsing using the Semantic Kernel.");
+            rootCommand.AddCommand(receiptCommand);
 
             try
             {
                 rootCommand.Invoke(args);
+                if (settings == null || imageLocal == null)
+                {
+                    throw new FileNotFoundException("Unable to find settings or image.");
+                }
             }
             catch (Exception ex)
             {
@@ -48,25 +52,25 @@ namespace SKFormParser
             }
 
             IKernel kernel = Kernel.Builder.Build();
-            if (settings!.Type == "azure_openai")
+            if (settings.Type == "azure_openai" && settings.AzureOpenAI != null)
             {
                 kernel.Config.AddAzureOpenAICompletionBackend(
-                    settings.AzureOpenAI!.DeploymentLabel,
+                    settings.AzureOpenAI.DeploymentLabel,
                     settings.AzureOpenAI.Model,
                     settings.AzureOpenAI.Endpoint, 
                     settings.AzureOpenAI.Key);
             } 
-            else if (settings?.Type == "openai")
+            else if (settings.Type == "openai" && settings.OpenAI != null)
             {
                 kernel.Config.AddOpenAICompletionBackend(
-                    settings.OpenAI!.Model,
+                    settings.OpenAI.Model,
                     settings.OpenAI.Model,
                     settings.OpenAI.Key, 
                     settings.OpenAI.Organization);
             }
             else
             {
-                Console.WriteLine();
+                Console.WriteLine("Please correctly specify a completion backend.");
                 Environment.Exit(1);
             }
 
@@ -81,9 +85,8 @@ namespace SKFormParser
                 SkillsUtils.GetSkillsFolderPath(),
                 "FormParsingSkill");
 
-            var pathToFile = @"C:\Users\taochen\Downloads\MicrosoftTeams-image.png";
             var result = await kernel.RunAsync(
-                pathToFile,
+                imageLocal.FullName,
                 recognizerSkill["RunRecognitionOnLocalFile"],
                 formParsingSkill["ParseReceipt"]);
 
